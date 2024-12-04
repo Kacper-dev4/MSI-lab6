274657b8804b8ecf6b4448db037f7e5aad6c8d85
Kacper Wach <wach.kacper8@gmail.com>
2024-12-04 12:54:01 +0100

    cokolwiek

--- lab6.m
+++ lab6.m
@@ -1,5 +1,5 @@
 clear all
-clc
+clc  
 close all
 
 %przepisy = readtable('przepisy.csv', 'VariableNamingRule', 'preserve');
@@ -17,6 +17,9 @@ end
 sklad = przepisy(:,3:32);
 sklad = table2array(sklad);
 sklad = double(sklad);
+
+wielkoscPunktow = 30;
+
 %% dane oryginalne
 % h = heatmap(skladniki,przepisy.Nazwa, sklad{:,:});
 
@@ -34,10 +37,11 @@ k = 4;
 [idx, C] = kmeans(sklad,k);
 [coeff, score, ~] = pca(sklad);
 
-scatter(score(:,1),score(:,2),50,idx,'filled');
+figure
+scatter(score(:,1),score(:,2),wielkoscPunktow,idx,'filled');
 xlabel('PCA 1');
 ylabel('PCA 2');
-title('Klasteryzacja k-means na PCA');
+title('Klasteryzacja dane oryginalne');
 grid on;
 
 %% dane zbinaryzowane
@@ -50,45 +54,91 @@ zbinPrzepisy = przepisy;
 figure
 hBin = heatmap(skladniki,przepisy.Nazwa, zbinSklad);
 
-dBin = pdist(zbinSklad);
+dBin = pdist(zbinSklad,'euclidean');
 
-Zbin = linkage(dBin,'ward');
+Zbin = linkage(dBin,'complete');
 
 figure
 dendrogram(Zbin);
+title('Dendrogram dane zbinaryzowane')
 
 [idxBin, Cbin] = kmeans(sklad,k);
 [coeffBin, scoreBin, ~] = pca(zbinSklad);
 
-scatter(scoreBin(:,1),scoreBin(:,2),50,idxBin,'filled');
+figure
+scatter(scoreBin(:,1),scoreBin(:,2),wielkoscPunktow,idxBin,'filled');
 xlabel('PCA 1');
 ylabel('PCA 2');
-title('Klasteryzacja k-means na PCA');
+title('Klasteryzacja dane zbinaryzowane');
 grid on;
 
 %% 5 składników
 
 sklad5 = [sklad(:,1), sklad(:,2), sklad(:,19), sklad(:,25), sklad(:,26)];
-
+przepisy5 = [przepisy(:,1), przepisy(:,2),przepisy(:,3), przepisy(:,4), przepisy(:,21), przepisy(:,27), przepisy(:,28)];
 figure
 h5 = heatmap(wybrane,przepisy.Nazwa, sklad5);
 
-d5 = pdist(sklad5);
+d5 = pdist(sklad5,'euclidean');
 
-Z5 = linkage(d5,'ward');
+Z5 = linkage(d5,'complete');
 
 figure
 dendrogram(Z5);
-
+title('Dendrogram dane dla 5 składników')
 
 [idx5, C5] = kmeans(sklad5,k);
 [coeff5, score5, ~] = pca(sklad5);
 
-scatter(score5(:,1),score5(:,2),50,idx5,'filled');
+figure
+scatter(score5(:,1),score5(:,2),wielkoscPunktow,idx5,'filled');
 xlabel('PCA 1');
 ylabel('PCA 2');
-title('Klasteryzacja k-means na PCA');
+title('Klasteryzacja dane dla 5 składników');
 grid on;
+
+% Zad3 
+
+%% a
+de = pdist(sklad,'euclidean');
+Ze = linkage(de,'complete');
+
+dM = pdist(sklad,'cityblock');
+ZM = linkage(dM,'complete');
+
+figure
+dendrogram(Ze);
+title('Dendrogram odległość euklidesowa')
+
+figure
+dendrogram(ZM);
+title('Dendrogram odległość Manhattan')
+
+%% b
+
+Zupgma = linkage(de,'average');
+Zsingle = linkage(de,'single');
+Zupgmc = linkage(de,'centroid'); 
+
+
+figure
+dendrogram(Ze)
+title('Dendrogram połączenie complete')
+
+
+figure
+dendrogram(Zupgma)
+title('Dendrogram połączenie UPGMA')
+
+figure
+dendrogram(Zsingle)
+title('Dendrogram połączenie single')
+
+figure
+dendrogram(Zupgmc)
+title('Dendrogram połączenie UPGMC')
+
+
 
 
 
